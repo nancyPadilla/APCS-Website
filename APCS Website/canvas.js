155a3047c61e180d0a6c04ef71220c1b441be3f1
nancyPadilla <39380027+nancyPadilla@users.noreply.github.com>
2018-05-17 08:41:09 -0700

    Add files via upload

---
+++ APCS Website/canvas.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link href="mystyle.css" rel="stylesheet" tyoe="text/css">
+		
+	</head>
+		<style>
+		body {background-image: url("https://static.pexels.com/photos/457882/pexels-photo-457882.jpeg");
+		</style>
+	<body>
+		<div id = "wrapperDiv">	<img src = "http://www.netanimations.net/animated-fish-jumping-out-of-water.gif">
+			<div id = "headerDiv">
+			<p class="title">A S P I R A T I O N S</p>
+			<div id = "navDiv">
+					  <a href = "index.html"><div class = "buttDiv">Home</div></a> 	
+					  <a href = "koala.html"><div class = "buttDiv">Animal</div></a>  
+					  <a href = "explore.html"><div class = "buttDiv">Explore</div></a>
+					  <a href = "chispe.html"><div class = "buttDiv">Doggo</div></a>
+					  <a href = "aspirations.html"><div class = "buttDiv">Aspiration</div></a>
+				      <a href = "canvas.js"><div class = "buttDiv">Java</div></a>
+					  </div>
+			<div id = "bodyDiv"><p class="body">
+			</div>
+			<div id = "footerDiv"></div>
+		</div>
+	</body>
+</html>
---
+++ APCS Website/sketch.js
@@ -0,0 +1,26 @@
+
+var xLoc, yLoc, xSpd, ySpd;
+
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(5, 5, 5);
+  fill(200, 30, 150);
+  xLoc = 200;
+  yLoc = 400;
+  xSpd = -1;
+  ySpd = 1;
+  
+}
+
+function draw() {
+	//background(5, 5, 5, 2);
+	fill(3, 3, 3, 3);
+	rect(0,0,width, height);
+	fill(200, 130, 150);
+	xLoc = xLoc + xSpd;
+	yLoc = yLoc + ySpd;
+     rect(xLoc, yLoc, 30, 30);
+}
+
+
